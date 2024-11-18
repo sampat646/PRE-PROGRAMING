ed45719cfa47bbeb39b32d9ffd95a4f17d59345b
sampat karehonna <sampatkarehonna@gmail.com>
2024-11-18 16:16:51 +0530

    getday pgm

---
+++ Practice questions/getday.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class getday {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the day number");
+        int inp = sc.nextInt();
+
+        if (inp == 1) {
+            System.out.println("Sunday");
+        } else if (inp == 2) {
+            System.out.println("Monday");
+        } else if (inp == 3) {
+            System.out.println("Tuesday");
+
+        } else if (inp == 4) {
+            System.out.println("Wednesday");
+
+        } else if (inp == 5) {
+            System.out.println("thursday");
+
+        } else if (inp == 6) {
+            System.out.println("Friday");
+
+        } else {
+            System.out.println("saturday");
+
+        }
+    }
+}
