efc2668d5eb003156f38be5b2ff8dcc22321a8c8
sampat karehonna <sampatkarehonna@gmail.com>
2024-11-19 15:44:28 +0530

    19-nov

---
+++ Practice questions/GetdatesOfMonth.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class GetdatesOfMonth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int num = sc.nextInt();
+
+        if (num == 1 || num == 3 || num == 5 || num == 7 || num == 8 || num == 10 || num == 12)
+            System.out.println("31 days are there");
+        else if (num == 4 || num == 6 || num == 9 || num == 11)
+            System.out.println("30 days arre there");
+        else if (num == 2)
+            System.out.println("28 days arre there");
+        else
+            System.out.println("Enter vaild number");
+
+    }
+}
